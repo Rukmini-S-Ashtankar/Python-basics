5c12ca8efb1556ce2272c5fe3c0f20e34f3599eb
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-07-24 00:47:44 +0530

    Add data_type.py to demonstrate variable types

---
+++ data_type.py
@@ -0,0 +1,13 @@
+a = 100
+b = 3.14
+c = "Python Programming"
+d = True
+e = [1,4,8,9]
+f = (10,25,45,47)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
