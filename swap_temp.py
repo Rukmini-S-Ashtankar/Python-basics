3406ee7fff1ab98b52d99647abccedabdac589ff
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-07-30 17:28:30 +0530

    Swapping value of two variables using temporary variable

---
+++ swap_temp.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter tthe second number: "))
+
+temp = a
+a = b
+b = temp
+
+print("After swapping:")
+print("First number: ", a)
+print("Second number: ", b)
