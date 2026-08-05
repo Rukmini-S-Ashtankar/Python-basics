30eae901493b3a1864e14ffb0b041252e2934141
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-08-05 22:00:52 +0530

    Add nested list example for student records

---
+++ 03_Lists/19_nested_list.py
@@ -0,0 +1,8 @@
+students = [
+  ["Rukmini", 22],
+  ["Abhi", 23],
+  ["Tanu", 21]
+]
+print("First Student: ", students[0])
+print("Second Student's Name: ", students[1][0])
+print("Third Student's Age: ", students[2][1])
