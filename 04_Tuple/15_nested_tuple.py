12ed2e43191a9ee9dcc4b3864671b446aa1336ff
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-08-07 19:06:11 +0530

    Add nested tuple example for student data

---
+++ 04_Tuple/15_nested_tuple.py
@@ -0,0 +1,9 @@
+students = (
+  ("Rukmini", 22),
+  ("Tanu", 23),
+  ("Tanvi", 21)
+)
+
+print("First Student:", students[0])
+print("Second Student's Name:", students[1][0])
+print("Third Student's Age:", students[2][1])
