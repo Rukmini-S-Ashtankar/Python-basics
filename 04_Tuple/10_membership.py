4b1d8a4a3d9c48f18ebc6e69b52cf184033c5bbd
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-08-07 18:34:27 +0530

    Add membership check for fruits in tuple

---
+++ 04_Tuple/10_membership.py
@@ -0,0 +1,8 @@
+fruits = ("Apple", "Banana", "Mango", "Orange", "Grape", "Watermelon")
+
+fruit = input("Enter a fruit: ")
+
+if fruit in fruits:
+  print("Fruit Found")
+else:
+  print("Fruit Not Found")
