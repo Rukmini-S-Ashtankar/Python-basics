197bc3acb6edbda2b81e635197b28471655038b8
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-08-10 22:54:33 +0530

    Add set operations example in 21_set_operations.py

---
+++ 05_Sets/21_set_operations.py
@@ -0,0 +1,10 @@
+set1 = {10, 20, 30, 40}
+set2 = {30, 40, 50, 60}
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+
+print("Union:", set1 | set2)
+print("Intersection:",  set1 & set2)
+print("Difference:", set1 - set2)
+print("Symmetric Difference:", set1 ^ set2)
