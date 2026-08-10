1b41df66fa67ec1e9ce5702490eecb4a2ed73602
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-08-10 22:48:25 +0530

    Add script to remove duplicates from a list

---
+++ 05_Sets/18_remove_duplicates.py
@@ -0,0 +1,7 @@
+numbers = [10, 20, 10, 30, 20, 40, 30]
+
+unique_numbers = list(set(numbers))
+
+print("Original List:", numbers)
+print("Without Duplicates:", unique_numbers)
+# list to set,  set to list
