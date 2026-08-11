0d2cbc1d13d6d46b71e4790d36d876262dab9704
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-08-11 23:43:21 +0530

    Add user input dictionary for student details

---
+++ 06_Dictionary/07_user_input_dictionary.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+course = input("Enter your course: "))
+
+student = {
+  "name": name,
+  "age": age,
+  "course": course
+}
+
+print("Student Details:", student)
