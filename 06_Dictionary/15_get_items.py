c983656b2f8d753e9e013c692561f01483b00dfb
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-08-12 22:35:52 +0530

    Add item retrieval and printing for student dictionary
    Iterate through student dictionary items and print each.

---
+++ 06_Dictionary/15_get_items.py
@@ -0,0 +1,9 @@
+student = {
+  "name": "Rukmini",
+  "age": 22,
+  "course": "MCA"
+}
+
+# print("Items:", student.items())
+for item in student.items():
+  print(item)                                # cleaner o/p
