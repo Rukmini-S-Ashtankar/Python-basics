81ff1be85a1acbd4922ca3a6350bd01295df6118
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-08-12 22:21:25 +0530

    Add key existence check in student dictionary

---
+++ 06_Dictionary/11_check_key.py
@@ -0,0 +1,12 @@
+student = {
+  "name": "Rukmini",
+  "age": 22,
+  "course": "MCA"
+}
+
+key = input("Enter key to search: ")
+
+if key in student:
+  print("Key exists.")
+else:
+  print("Key does not exist.")
