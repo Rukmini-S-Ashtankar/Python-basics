64e18d3bde1ba78f04cd278a11e6e4835a310d5d
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-08-13 23:50:19 +0530

    Add nested dictionary for student information

---
+++ 06_Dictionary/18_nested_dictionary.py
@@ -0,0 +1,12 @@
+students = {
+  "student1": {
+    "name": "Rukmini",
+    "age": 22
+  },
+  "student2": {
+    "name": "Tanu",
+    "age": 23
+  }
+}
+print("Student 1:", students["student1"])
+print("Student 2 Name:", students["student2"]["name"])
