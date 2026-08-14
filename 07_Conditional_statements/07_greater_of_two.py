bff9b1efcd31b12c18dba0db45c8b6d26c1d44aa
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-08-14 22:32:23 +0530

    Implement greater_of_two.py for number comparison

---
+++ 07_Conditional_statements/07_greater_of_two.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+  print("Greater:", a)
+if b > a:
+  print("Greater:", b)
+else:
+  print("Both numbers are equal.")
