5d8384cfc3a900c8499197a3408158e8180277ce
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-08-24 23:31:27 +0530

    Add recursive function to reverse a string

---
+++ 10_Recursion/05_reverse_string.py
@@ -0,0 +1,11 @@
+def reverse_string(text):
+  if len(text) == 0:
+    return ""
+    
+  return reverse_string(text[1:]) + text[0]
+
+
+text = input("Enter a string: ")
+
+print("Reversed:", reverse_string(text))
+  
