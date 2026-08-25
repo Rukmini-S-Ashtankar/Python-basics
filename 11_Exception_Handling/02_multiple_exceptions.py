9171903339ef9794c2a3f7deec90296cf56ab489
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-08-25 23:39:02 +0530

    Implement multiple exception handling in script

---
+++ 11_Exception_Handling/02_multiple_exceptions.py
@@ -0,0 +1,11 @@
+try:
+  number = int(input("Enter a number: "))
+  result = 10 / number
+  print("Result: ", result)
+
+except ValueError:
+  print("Please enter a valid number.")
+
+except ZeroDivisionError:
+  print("Cannot divide by Zero.")
+  
