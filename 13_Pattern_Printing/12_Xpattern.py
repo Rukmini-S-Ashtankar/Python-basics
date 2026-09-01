5b1bec59f290e0df783c70211e755e0e75b190c9
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-09-01 23:33:28 +0530

    Add X pattern printing in 12_Xpattern.py

---
+++ 13_Pattern_Printing/12_Xpattern.py
@@ -0,0 +1,9 @@
+rows = 5
+
+for i in range(rows):
+    for j in range(rows):
+        if j == i or j == rows - i - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
