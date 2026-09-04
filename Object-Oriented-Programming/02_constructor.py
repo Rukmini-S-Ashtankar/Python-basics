d3545ce23e508ab6d3fe74683c0ffa56e1976505
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-09-04 23:20:35 +0530

    Add Student class with constructor and example usage

---
+++ Object-Oriented-Programming/02_constructor.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+student1 = Student("Rukmini", 22)
+
+print("Name:", student1.name)
+print("Age:", student1.age)
