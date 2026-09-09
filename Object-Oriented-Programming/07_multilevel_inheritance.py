1a146d3deb0b477a422648792326332a2e306a98
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-09-09 20:03:48 +0530

    Implement multilevel inheritance with Animal, Dog, and Puppy classes

---
+++ Object-Oriented-Programming/07_multilevel_inheritance.py
@@ -0,0 +1,20 @@
+class Animal:
+    def eat(self):
+        print("Eating")
+
+
+class Dog(Animal):
+    def bark(self):
+        print("Barking")
+
+
+class Puppy(Dog):
+    def play(self):
+        print("Playing")
+
+
+puppy = Puppy()
+
+puppy.eat()
+puppy.bark()
+puppy.play()
